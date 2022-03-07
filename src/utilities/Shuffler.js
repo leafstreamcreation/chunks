12aca5080bf57566cf919d8f15ea23326972c3e8
Derek <shouldergiant@icloud.com>
2022-03-06 21:45:00 -0500

    first commit

---
+++ src/utilities/Shuffler.js
@@ -0,0 +1,23 @@
+
+
+class Shuffler {
+    constructor(array) {
+      this.elements = array ? [...array] : [];
+      let remainingElements = this.elements.length,
+        elementToSwap,
+        nextElementIndex;
+  
+      while (remainingElements > 0) {
+        nextElementIndex = Math.floor(Math.random() * remainingElements--);
+        elementToSwap = this.elements[remainingElements];
+        this.elements[remainingElements] = this.elements[nextElementIndex];
+        this.elements[nextElementIndex] = elementToSwap;
+      }
+    }
+  
+    drawNext() {
+      return this.elements.pop();
+    }
+  }
+
+  export default Shuffler
