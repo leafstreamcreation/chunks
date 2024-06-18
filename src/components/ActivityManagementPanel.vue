3b94adb5b78108b19ce8c43c98d71a38fa8766ec
Derek Noble <shouldergiant@icloud.com>
2024-06-18 12:39:15 -0700

    extract ActivityManagementPanel logic from Do.vue

--- src/components/ActivityManagementPanel.vue
+++ src/components/ActivityManagementPanel.vue
@@ -2,11 +2,95 @@
 //this component manages the list of activities for the current category
 //it has a list of activities for the category,
 //a field to add new activities to the category,
+
+import { reactive } from "vue";
+import { useActivityStore } from "../stores/activityStore";
+
 //and controls to edit and delete selected activities in the category
+const activityStore = useActivityStore();
+
+const state = reactive({
+  editingActivities: false,
+  nameInProgress: "",
+  selectedId: null,
+});
+
+function selectActivity(id) {
+  state.selectedId = state.selectedId !== id ? id : null;
+  const thisActivity = activityStore.activitiesInView[id];
+  state.nameInProgress = state.selectedId ? thisActivity.name : "";
+}
+
+function clearSelected() {
+  state.nameInProgress = "";
+  state.selectedId = null;
+}
+
+async function deleteActivity(id) {
+  await activityStore.deleteActivity(id);
+  clearSelected();
+}
+
+async function createActivity() {
+  await activityStore.createActivity(
+    state.nameInProgress,
+    activityStore.activeCategory
+  );
+  clearSelected();
+}
+
+async function updateActivity(id) {
+  const activity = await activityStore.renameActivity(id, state.nameInProgress);
+  if (activity) clearSelected();
+}
+
+function changeNewActivityText({ target }) {
+  state.nameInProgress = target.value;
+}
 </script>
 
 <template>
-  <div />
+  <div class="management-panel" @click="clearSelected">
+    <h3
+      :class="
+        state.editingActivities
+          ? 'management-title-collapsed'
+          : 'management-title'
+      "
+      @click="state.editingActivities = !state.editingActivities"
+    >
+      Manage
+    </h3>
+    <div v-if="state.editingActivities" class="new-activity text-div">
+      <input
+        type="text"
+        name="name"
+        :value="state.selectedId ? '' : state.nameInProgress"
+        @input="changeNewActivityText"
+      />
+      <button @click.stop="createActivity">Add</button>
+    </div>
+    <div v-if="state.editingActivities" class="activity-list">
+      <div
+        v-for="{ id, name } in activityStore.activitiesInView"
+        :key="id"
+        class="list-item"
+      >
+        <div v-if="state.selectedId === id" class="activity-index text-div">
+          <button @click.stop="deleteActivity(id)">-</button>
+          <input
+            type="text"
+            name="update-name"
+            v-model="state.nameInProgress"
+            @click.stop=""
+            @change="updateActivity(id)"
+          />
+          <button>Rename</button>
+        </div>
+        <p v-else @click.stop="selectActivity(id)">{{ name }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -15,4 +99,51 @@
 /* Columbia blue #BDD5EA; */
 /* Ghost white   #F7F7FF; */
 /* Jasmine       #F6E27F; */
+.management-panel {
+  height: 30%;
+  background-color: #577399;
+  color: black;
+}
+
+.management-title {
+  height: 100%;
+  background-color: #bdd5ea;
+}
+
+.management-title-collapsed {
+  background-color: #bdd5ea;
+}
+
+.activity-index {
+  display: flex;
+  justify-content: center;
+}
+
+.new-activity {
+  display: flex;
+  justify-content: center;
+}
+.activity-list {
+  height: 50%;
+  overflow-y: scroll;
+}
+
+.list-item {
+  height: 30px;
+  background-color: #f6e27f;
+  display: flex;
+  justify-content: center;
+}
+
+h1,
+h3 {
+  text-align: center;
+  color: black;
+}
+
+.text-div {
+  height: 30px;
+  display: flex;
+  flex-direction: row;
+}
 </style>
--- src/views/Do.vue
+++ src/views/Do.vue
@@ -10,9 +10,6 @@ const activityStore = useActivityStore();
 const state = reactive({
   runStarted: false,
   activityLocked: true,
-  editingActivities: false,
-  nameInProgress: "",
-  selectedId: null,
 });
 
 onMounted(async () => {
@@ -20,8 +17,8 @@ onMounted(async () => {
 });
 
 function updateCategory() {
-  state.editingActivities = false;
-  clearSelected();
+  // state.editingActivities = false;
+  // clearSelected();
 }
 
 function updateCurrentActivity(activity) {
@@ -29,39 +26,6 @@ function updateCurrentActivity(activity) {
     addHistoryRecord();
   activityStore.runningActivity =
     activity !== activityStore.runningActivity ? activity : {};
-}
-
-function selectActivity(id) {
-  state.selectedId = state.selectedId !== id ? id : null;
-  const thisActivity = activityStore.activitiesInView[id];
-  state.nameInProgress = state.selectedId ? thisActivity.name : "";
-}
-
-function clearSelected() {
-  state.nameInProgress = "";
-  state.selectedId = null;
-}
-
-async function deleteActivity(id) {
-  await activityStore.deleteActivity(id);
-  clearSelected();
-}
-
-async function createActivity() {
-  await activityStore.createActivity(
-    state.nameInProgress,
-    activityStore.activeCategory
-  );
-  clearSelected();
-}
-
-async function updateActivity(id) {
-  const activity = await activityStore.renameActivity(id, state.nameInProgress);
-  if (activity) clearSelected();
-}
-
-function changeNewActivityText({ target }) {
-  state.nameInProgress = target.value;
 }
 
 async function addHistoryRecord() {
@@ -74,7 +38,7 @@ async function addHistoryRecord() {
   <!-- current activity -->
   <!-- activity selection -->
   <!-- management panel -->
-  <div class="do-bounds" @click="clearSelected">
+  <div class="do-bounds">
     <CategoryBar @nextCategory="updateCategory" />
     <div class="current-activity">
       <div
@@ -92,47 +56,7 @@ async function addHistoryRecord() {
       :runningActivity="activityStore.runningActivity"
       @activitySelected="updateCurrentActivity"
     />
-    <div class="management-panel">
-      <h3
-        :class="
-          state.editingActivities
-            ? 'management-title-collapsed'
-            : 'management-title'
-        "
-        @click="state.editingActivities = !state.editingActivities"
-      >
-        Manage
-      </h3>
-      <div v-if="state.editingActivities" class="new-activity text-div">
-        <input
-          type="text"
-          name="name"
-          :value="state.selectedId ? '' : state.nameInProgress"
-          @input="changeNewActivityText"
-        />
-        <button @click.stop="createActivity">Add</button>
-      </div>
-      <div v-if="state.editingActivities" class="activity-list">
-        <div
-          v-for="{ id, name } in activityStore.activitiesInView"
-          :key="id"
-          class="list-item"
-        >
-          <div v-if="state.selectedId === id" class="activity-index text-div">
-            <button @click.stop="deleteActivity(id)">-</button>
-            <input
-              type="text"
-              name="update-name"
-              v-model="state.nameInProgress"
-              @click.stop=""
-              @change="updateActivity(id)"
-            />
-            <button @click.stop="">Rename</button>
-          </div>
-          <p v-else @click.stop="selectActivity(id)">{{ name }}</p>
-        </div>
-      </div>
-    </div>
+    <ActivityManagementPanel />
   </div>
 </template>
 
@@ -168,48 +92,5 @@ div {
 .current-activity {
   background-color: #f6e27f;
   height: 20%;
-}
-
-.management-panel {
-  height: 30%;
-  background-color: #577399;
-  color: black;
-}
-
-.management-title {
-  height: 100%;
-  background-color: #bdd5ea;
-}
-
-.management-title-collapsed {
-  background-color: #bdd5ea;
-}
-
-.activity-list {
-  height: 50%;
-  overflow-y: scroll;
-}
-
-.text-div {
-  height: 30px;
-  display: flex;
-  flex-direction: row;
-}
-
-.list-item {
-  height: 30px;
-  background-color: #f6e27f;
-  display: flex;
-  justify-content: center;
-}
-
-.activity-index {
-  display: flex;
-  justify-content: center;
-}
-
-.new-activity {
-  display: flex;
-  justify-content: center;
 }
 </style>
